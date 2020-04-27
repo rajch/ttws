72f164ecec7faa7f6acf4897e8371408177b8724
Raj Chaudhuri <rajch@hotmail.com>
2020-04-27 17:03:49 +0530

    Made ics coding style same as ldgen

--- cmd/ics/main.go
+++ cmd/ics/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	_ "github.com/rajch/ttws/pkg/envvars"
 	_ "github.com/rajch/ttws/pkg/filesystem"
-	_ "github.com/rajch/ttws/pkg/ipaddresses"
+	"github.com/rajch/ttws/pkg/ipaddresses"
 	"github.com/rajch/ttws/pkg/webserver"
 )
 
 func main() {
-	webserver.SetRootHandler("/ip")
+	webserver.SetRootHandler(ipaddresses.DefaultPath)
 	webserver.ListenAndServe()
 }
