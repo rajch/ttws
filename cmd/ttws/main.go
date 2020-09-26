c6d912bbd14f3afa6fb4b8611f7bd3aea69ef348
Raj Chaudhuri <rajch@hotmail.com>
2020-09-26 14:05:51 +0530

    ttws cmd modified due to changes in probes

--- cmd/ttws/main.go
+++ cmd/ttws/main.go
@@ -5,10 +5,12 @@ import (
 	_ "github.com/rajch/ttws/pkg/envvars"
 	_ "github.com/rajch/ttws/pkg/filesystem"
 	_ "github.com/rajch/ttws/pkg/ipaddresses"
-	_ "github.com/rajch/ttws/pkg/probes"
+	"github.com/rajch/ttws/pkg/probes"
 	"github.com/rajch/ttws/pkg/webserver"
 )
 
 func main() {
+	probes.NewProbe("Readiness", 10, 0)
+	probes.NewProbe("Liveness", 10, 0)
 	webserver.ListenAndServe()
 }
